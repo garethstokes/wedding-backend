ba4ea6fd2c83d31bebab58fe26532702f53e1d5e
gareth <gareth@balancedlabs.com>
2016-03-20 20:26:29 +1100

    create procfile

--- Main.hs
+++ Main.hs
@@ -2,8 +2,14 @@
 
 import Web.Scotty
 import Network.Wai.Middleware.Static
+import System.Environment (lookupEnv)
+
+lookupSetting :: Read a => String -> a -> IO a
+lookupSetting env def = maybe def read <$> lookupEnv env
 
 main :: IO ()
-main = scotty 3000 $ do
-  middleware $ staticPolicy (noDots >-> addBase "dist/")
-  get "/" $ file "dist/index.html"
+main = do
+  port <- lookupSetting "PORT" 3000
+  scotty port $ do
+    middleware $ staticPolicy (noDots >-> addBase "dist/")
+    get "/" $ file "dist/index.html"
